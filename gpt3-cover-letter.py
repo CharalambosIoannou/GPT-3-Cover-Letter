07eb74bd1ef0a11bbca67903dc7a6dbee7239c94
Charalambos Ioannou <45453951+CharalambosIoannou@users.noreply.github.com>
2022-04-27 10:07:40 +0100

    Update gpt3-cover-letter.py

--- gpt3-cover-letter.py
+++ gpt3-cover-letter.py
@@ -12,16 +12,13 @@ with open("prompt.txt") as pro:
 # The Model
 returns = ai.Completion.create(
     engine="davinci", # OpenAI has made four text completion engines available, named davinci, ada, babbage and curie. We are using davinci, which is the most capable of the four.
-	prompt=prompt, # The text we will use as input
-  max_tokens=100, # maximum completion length.
-	temperature=0.9, # a number between 0 and 1 that determines how many creative risks the engine takes when generating text.,
-	top_p=1, # an alternative way to control the originality and creativity of the generated text.
-	n=1, #
-	frequency_penalty=0.5, # a number between 0 and 1. The higher this value the model will make a bigger effort in not repeating itself.
-	presence_penalty=0.9 # a number between 0 and 1. The higher this value the model will make a bigger effort in talking about new topics.
-	#stream=False,
-	#logprobs=Null,
-	#stop="\n"
+    prompt=prompt, # The text file we use as input (step 3)
+    max_tokens=100, # how many maximum characters the text will consists of.
+    temperature=0.9, # a number between 0 and 1 that determines how many creative risks the engine takes when generating text.,
+    top_p=1, # an alternative way to control the originality and creativity of the generated text.
+    n=1, # number of predictions to generate
+    frequency_penalty=0.5, # a number between 0 and 1. The higher this value the model will make a bigger effort in not repeating itself.
+    presence_penalty=0.9 # a number between 0 and 1. The higher this value the model will make a bigger effort in talking about new topics.
 )
 
 print(returns)
